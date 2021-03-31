f6c3c48441aad1afd785655efcd6b2f275b5b562
Shruti Tiwari <sh03shruti@gmail.com>
2021-03-31 21:18:17 +0530

    Cache bug

--- EPSILON 2021/assets/js/main.js
+++ EPSILON 2021/assets/js/main.js
@@ -103,7 +103,7 @@ sr.reveal(`.home__data, .home__img,
             .app__data, .app__img,
             .contact__data, .contact__button,
             .footer__content`, {
-    interval: 200
+    interval: 120
 })
 
 /*==================== TIMER ====================*/
